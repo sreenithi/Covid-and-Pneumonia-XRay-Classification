384d5333554df95d8093a6d5fe3c9025c593aeaf
Sreenithi <19sreenithi98@gmail.com>
2021-04-03 09:16:55 +0400

    added trial code for deployment

---
+++ score.py
@@ -0,0 +1,26 @@
+import json
+import os
+from keras.models import model_from_json
+from azure.core.model import Model
+import numpy as np
+
+def init():
+    global model
+
+    model_root_path = Model.get_model_path('covid-pneumonia-cnn')
+    
+    model_json_file = open(os.path.join(model_root_path, "model.json"), 'r')
+    model_json = model_json_file.read()
+
+    model_json_file.close()
+
+    model = model_from_json(model_json)
+
+    model.load_weights(os.path.join(model_root_path, 'model.h5'))
+    model.compile(loss="binary_crossentropy", optimizer='rmsprop', metrics=['accuracy'])
+ 
+    
+def run(data_gen):
+    # data = np.array(json.loads(raw_data)['data'])
+    y_hat = np.argmax(model.predict(data_gen), axis=1)
+    return y_hat.tolist()
